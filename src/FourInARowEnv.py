985c1e6a5a3d64d9409d76c5347ba61aebb94501
pizzaboy2 <44971776+pizzaboy2@users.noreply.github.com>
2021-12-23 08:30:52 +0100

    moved

--- src/FourInARowEnv.py
+++ src/FourInARowEnv.py
@@ -34,61 +34,28 @@ class FourInARowEnv:
   def get_possible_actions(self):
     pass
 
-  def _get_all_diagonals(self):
-    x = self._state.width
-    y = self._state.height
-    a = np.arange(x*y).reshape(x,y)
-    diags = [a[::-1,:].diagonal(i) for i in range(-a.shape[0]+1,a.shape[1])]
-    diags.extend(a.diagonal(i) for i in range(a.shape[1]-1,-a.shape[0],-1))
-    #from top! --> fix this last part
-    dia_arr = []
-    for diagonal_ar in diags:
-      new_line = []
-      for spot in diagonal_ar:
-        given_nr = spot 
-
-        # gives column and row nr from top --> this might cause issues???
-        column_nr = given_nr // self._state.height
-        row_nr = given_nr % self._state.height
-
-        row_nr_fixed = (y-row_nr) -1
-
-        
-        new_line.append(self._state.get_grid()[column_nr][row_nr_fixed])
-      dia_arr.append(new_line)
-    return dia_arr
-
 
   def is_done(self) -> bool:
     is_done = False
     allowed_winners = [BoxState.RED,BoxState.YELLOW]
     for colour in allowed_winners:
-      if self.horizontal_win(colour) or self.vertical_win(colour) or self.diagonal_win(colour):
+      if self._horizontal_win(colour) or self._vertical_win(colour) or self._diagonal_win(colour):
         is_done = True
     return is_done
 
-  def diagonal_win(self,colour) -> bool:
-    for diagonal in self._get_all_diagonals():
-      if self.is_diag_valid(diagonal,colour):
-        return True
-    return False
 
-  def is_diag_valid(self, diag, colour) -> bool:
-    streak = 0 
-    for spot in diag:
-      if spot == colour:
-        streak +=1
-        if streak ==4:
-          return True
-      else:
-        streak = 0
-    return False
-    
+  def get_reward(self):
+    pass
 
-# https://stackoverflow.com/questions/6313308/get-all-the-diagonals-in-a-matrix-list-of-lists-in-python
+  def get_transition_prob(self, action):
+    pass
+
+
+
+# Is done methods
 
   # Code Quality should be fixed
-  def vertical_win(self,colour) -> bool:
+  def _vertical_win(self,colour) -> bool:
     streak = 0 
     for i in range(self._state.width):
         for j in range(self._state.height):   
@@ -100,7 +67,7 @@ class FourInARowEnv:
             streak = 0
     return False
 
-  def horizontal_win(self,colour) -> bool:
+  def _horizontal_win(self,colour) -> bool:
     streak = 0 
     for i in range(self._state.height):
         for j in range(self._state.width):   
@@ -112,8 +79,42 @@ class FourInARowEnv:
             streak = 0
     return False
 
-  def get_reward(self):
-    pass
+  def _diagonal_win(self,colour) -> bool:
+    for diagonal in self._get_all_diagonals():
+      if self._is_diag_valid(diagonal,colour):
+        return True
+    return False
 
-  def get_transition_prob(self, action):
-    pass
+  def _get_all_diagonals(self):
+    x = self._state.width
+    y = self._state.height
+
+    a = np.arange(x*y).reshape(x,y)
+    diags = [a[::-1,:].diagonal(i) for i in range(-a.shape[0]+1,a.shape[1])]
+    diags.extend(a.diagonal(i) for i in range(a.shape[1]-1,-a.shape[0],-1))
+
+    dia_arr = []
+    for diagonal_ar in diags:
+      new_line = []
+      for spot in diagonal_ar:
+        given_nr = spot 
+        column_nr = given_nr // self._state.height
+        row_nr = given_nr % self._state.height
+        row_nr_fixed = (y-row_nr) -1
+        new_line.append(self._state.get_grid()[column_nr][row_nr_fixed])
+      dia_arr.append(new_line)
+    return dia_arr
+
+# https://stackoverflow.com/questions/6313308/get-all-the-diagonals-in-a-matrix-list-of-lists-in-python
+
+  def _is_diag_valid(self, diag, colour) -> bool:
+    streak = 0 
+    for spot in diag:
+      if spot == colour:
+        streak +=1
+        if streak ==4:
+          return True
+      else:
+        streak = 0
+    return False
+    
