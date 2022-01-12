c990e61c7f90f7c0a23e469e9d4ef204e21c4a67
Taran van Groenigen <tvangroenigen@outlook.com>
2022-01-12 16:57:32 +0100

    code now runs

--- src/FourInARowEnv.py
+++ src/FourInARowEnv.py
@@ -17,12 +17,14 @@ def player_from_box_state(box_state: BoxState) -> Optional[Players]:
 class FourInARowEnv():
   _state   : FourInARowState
   _renderer: FourInARowRenderer
-  _yellow_agent = FourInARowAgent
+  _yellow_agent: FourInARowAgent
+  _possible_states: Optional[list[FourInARowState]]
 
   def __init__(self, yellow_agent, width: int = 7, height: int = 6, first_turn: Players = Players.RED, win_condition: int = 4) -> None:
     self._state    = FourInARowState(width=width, height=height, first_turn=first_turn, win_condition=win_condition)
     self._renderer = FourInARowRenderer(self._state)
     self._yellow_agent = yellow_agent(self)
+    self._possible_states = None
 
   def _calculate_possible_states(self, state: FourInARowState) -> list[FourInARowState]:
     possible_states = []
@@ -59,7 +61,10 @@ class FourInARowEnv():
     return self._renderer.render()
 
   def get_possible_states(self) -> list[FourInARowState]:
-    return self._calculate_possible_states(self._state)
+    if self._possible_states == None:
+      self._possible_states = self._calculate_possible_states(self._state)
+
+    return self._possible_states
 
   def get_possible_actions(self, state: Optional[FourInARowState] = None) -> list[int]:
     if state is None:
